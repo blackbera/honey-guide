2e14e3ab2ebc60f2b419e94c40b73f41ca43e5e7
black bera <18746720+adam0x7@users.noreply.github.com>
2024-12-18 16:19:16 -0500

    testing

--- src/HoneyMinter.sol
+++ src/HoneyMinter.sol
@@ -1,24 +1,23 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
+import {ERC20} from "solady/src/tokens/ERC20.sol";
 import {HoneyFactory} from "../lib/contracts-monorepo/src/honey/HoneyFactory.sol";
 import {Honey} from "../lib/contracts-monorepo/src/honey/Honey.sol";
-import {ERC20} from "solady/src/tokens/ERC20.sol";
-
-/// @title HoneyTutorial
-/// @notice A simple contract demonstrating how to interact with Honey stablecoin
-/// @dev This is for educational purposes only
-contract HoneyTutorial {
-    HoneyFactory public immutable factory;
-    Honey public immutable honey;
+/// @title HoneyMinter
+/// @notice A simple contract demonstrating how to interact with Berachain's Honey stablecoin
+/// @dev This is for educational purposes only not for production use
+contract HoneyMinter {
+    HoneyFactory public immutable FACTORY;
+    Honey public immutable HONEY;
 
     error InsufficientAllowance();
     error InsufficientBalance();
     error TransferFailed();
 
     constructor(address _factory, address _honey) {
-        factory = HoneyFactory(_factory);
-        honey = Honey(_honey);
+        FACTORY = HoneyFactory(_factory);
+        HONEY = Honey(_honey);
     }
 
     /// @notice Mints Honey tokens using a single collateral (non-basket mode)
@@ -41,36 +40,39 @@ contract HoneyTutorial {
         if (!success) revert TransferFailed();
 
         // Approve factory to spend collateral
-        ERC20(collateral).approve(address(factory), amount);
+        ERC20(collateral).approve(address(FACTORY), amount);
 
         // Mint Honey - note: we expect non-basket mode (false)
-        mintedAmount = factory.mint(collateral, amount, msg.sender, false);
+        mintedAmount = FACTORY.mint(collateral, amount, msg.sender, false);
     }
 
     /// @notice Redeems Honey tokens for a single collateral (non-basket mode)
     /// @param collateral The collateral token to receive
     /// @param honeyAmount The amount of Honey to redeem
     /// @return redeemedAmount The amount of collateral received
-    function redeemBasicMode(address collateral, uint256 honeyAmount) external returns (uint256[] memory redeemedAmount) {
+    function redeemBasicMode(address collateral, uint256 honeyAmount) 
+        external 
+        returns (uint256[] memory redeemedAmount) 
+    {
         // Check allowance
-        if (honey.allowance(msg.sender, address(this)) < honeyAmount) {
+        if (HONEY.allowance(msg.sender, address(this)) < honeyAmount) {
             revert InsufficientAllowance();
         }
 
         // Check balance
-        if (honey.balanceOf(msg.sender) < honeyAmount) {
+        if (HONEY.balanceOf(msg.sender) < honeyAmount) {
             revert InsufficientBalance();
         }
 
         // Transfer Honey to this contract
-        bool success = honey.transferFrom(msg.sender, address(this), honeyAmount);
+        bool success = HONEY.transferFrom(msg.sender, address(this), honeyAmount);
         if (!success) revert TransferFailed();
 
         // Approve factory to spend Honey
-        honey.approve(address(factory), honeyAmount);
+        HONEY.approve(address(FACTORY), honeyAmount);
 
         // Redeem Honey - note: we expect non-basket mode (false)
-        redeemedAmount = factory.redeem(collateral, honeyAmount, msg.sender, false);
+        redeemedAmount = FACTORY.redeem(collateral, honeyAmount, msg.sender, false);
     }
 
     /// @notice Mints Honey tokens using multiple collaterals (basket mode)
@@ -79,57 +81,66 @@ contract HoneyTutorial {
     /// @return mintedAmount The amount of Honey minted
     function mintBasketMode(address collateral, uint256 amount) external returns (uint256 mintedAmount) {
         // First check if basket mode is enabled for minting
-        require(factory.isBasketModeEnabled(true), "Basket mode not enabled");
+        require(FACTORY.isBasketModeEnabled(true), "Basket mode not enabled");
 
-        // Check allowance for reference collateral
-        if (ERC20(collateral).allowance(msg.sender, address(this)) < amount) {
-            revert InsufficientAllowance();
+        // Get all registered assets
+        uint256 numAssets = FACTORY.numRegisteredAssets();
+        for(uint256 i = 0; i < numAssets; i++) {
+            address asset = FACTORY.registeredAssets(i);
+            
+            // Check allowance for each asset
+            if (ERC20(asset).allowance(msg.sender, address(this)) < amount) {
+                revert InsufficientAllowance();
+            }
+
+            // Transfer asset to this contract
+            bool success = ERC20(asset).transferFrom(msg.sender, address(this), amount);
+            if (!success) revert TransferFailed();
+
+            // Approve factory to spend asset
+            ERC20(asset).approve(address(FACTORY), amount);
         }
 
-        // Transfer reference collateral to this contract
-        bool success = ERC20(collateral).transferFrom(msg.sender, address(this), amount);
-        if (!success) revert TransferFailed();
-
-        // Approve factory to spend collateral
-        ERC20(collateral).approve(address(factory), amount);
-
         // Mint Honey - note: we expect basket mode (true)
-        mintedAmount = factory.mint(collateral, amount, msg.sender, true);
+        mintedAmount = FACTORY.mint(collateral, amount, msg.sender, true);
     }
 
     /// @notice Redeems Honey tokens for multiple collaterals (basket mode)
     /// @param collateral The reference collateral token address
     /// @param honeyAmount The amount of Honey to redeem
     /// @return redeemedAmounts Array of redeemed collateral amounts
-    function redeemBasketMode(address collateral, uint256 honeyAmount) external returns (uint256[] memory redeemedAmounts) {
+    function redeemBasketMode(address collateral, uint256 honeyAmount) 
+        external 
+        returns (uint256[] memory redeemedAmounts) 
+    {
         // First check if basket mode is enabled for redeeming
-        require(factory.isBasketModeEnabled(false), "Basket mode not enabled");
+        require(FACTORY.isBasketModeEnabled(false), "Basket mode not enabled");
 
         // Check allowance
-        if (honey.allowance(msg.sender, address(this)) < honeyAmount) {
+        if (HONEY.allowance(msg.sender, address(this)) < honeyAmount) {
             revert InsufficientAllowance();
         }
 
         // Check balance
-        if (honey.balanceOf(msg.sender) < honeyAmount) {
+        if (HONEY.balanceOf(msg.sender) < honeyAmount) {
             revert InsufficientBalance();
         }
 
         // Transfer Honey to this contract
-        bool success = honey.transferFrom(msg.sender, address(this), honeyAmount);
+        bool success = HONEY.transferFrom(msg.sender, address(this), honeyAmount);
         if (!success) revert TransferFailed();
 
         // Approve factory to spend Honey
-        honey.approve(address(factory), honeyAmount);
+        HONEY.approve(address(FACTORY), honeyAmount);
 
         // Redeem Honey - note: we expect basket mode (true)
-        redeemedAmounts = factory.redeem(collateral, honeyAmount, msg.sender, true);
+        redeemedAmounts = FACTORY.redeem(collateral, honeyAmount, msg.sender, true);
     }
 
     /// @notice Checks if basket mode is enabled for minting or redeeming
     /// @param isMint True to check mint mode, false to check redeem mode
     /// @return True if basket mode is enabled
     function isBasketMode(bool isMint) external view returns (bool) {
-        return factory.isBasketModeEnabled(isMint);
+        return FACTORY.isBasketModeEnabled(isMint);
     }
 } 
--- test/HoneyTutorial.t.sol
+++ test/HoneyTutorial.t.sol
@@ -2,144 +2,128 @@
 pragma solidity ^0.8.19;
 
 import {Test, console2} from "forge-std/Test.sol";
-import {HoneyTutorial} from "../src/HoneyTutorial.sol";
+import {HoneyMinter} from "../src/HoneyMinter.sol";
 import {HoneyFactory} from "../lib/contracts-monorepo/src/honey/HoneyFactory.sol";
+import {HoneyFactoryReader} from "../lib/contracts-monorepo/src/honey/HoneyFactoryReader.sol";
 import {Honey} from "../lib/contracts-monorepo/src/honey/Honey.sol";
 import {ERC20} from "solady/src/tokens/ERC20.sol";
 
 contract HoneyTutorialTest is Test {
-    HoneyTutorial public tutorial;
+    HoneyMinter public minter;
     HoneyFactory public factory;
+    HoneyFactoryReader public factoryReader;
     Honey public honey;
     
-    // Mainnet addresses
-    address public constant USDC = 0x015fd589F4f1A33ce4487E12714e1B15129c9329;
-    
-    // Berachain roles
-    bytes32 public constant MANAGER_ROLE = 0x241ecf16d79d0f8dbfb92cbc07fe17840425976cf0667f022fe9877caa831b08;
-    address public constant MANAGER = address(0x4e59b44847b379578588920cA78FbF26c0B4956C);
-    address public user = address(2);
-
-    // Contract addresses
+    address public constant USDT = 0x015fd589F4f1A33ce4487E12714e1B15129c9329;
+    address public constant USDC = 0x164A2dE1bc5dc56F329909F7c97Bae929CaE557B;
     address public constant HONEY = 0xd137593CDB341CcC78426c54Fb98435C60Da193c;
     address public constant HONEY_FACTORY = 0xA81F0019d442f19f66880bcf2698B4E5D5Ec249A;
+    address public constant FACTORY_READER = 0x8C4A67395d60D235827F5edE446941E84d30a5B1;
+    
+    address public user = address(2);
 
     // Fork setup
     uint256 forkId;
 
     function setUp() public {
-        // Create a fork of Berachain
-        forkId = vm.createFork("https://rpc.berachain.com");
+        // Cartio fork
+        forkId = vm.createFork("https://rockbeard-eth-cartio.berachain.com");
         vm.selectFork(forkId);
         
         // Get the deployed contracts
         factory = HoneyFactory(HONEY_FACTORY);
+        factoryReader = HoneyFactoryReader(FACTORY_READER);
         honey = Honey(HONEY);
-
+        
         // Deploy our tutorial contract
-        tutorial = new HoneyTutorial(HONEY_FACTORY, HONEY);
+        minter = new HoneyMinter(HONEY_FACTORY, HONEY);
 
         // Deal some tokens to our test user
+        deal(USDT, user, 1000000 * 1e6);  // 1M USDT
         deal(USDC, user, 1000000 * 1e6);  // 1M USDC
-
-        // Setup necessary permissions
-        vm.startPrank(MANAGER);
-        factory.setMintRate(USDC, 0.98e18); // 98% mint rate
-        factory.setRedeemRate(USDC, 0.98e18); // 98% redeem rate
-        vm.stopPrank();
     }
 
     function test_BasicModeMinting() public {
         vm.startPrank(user);
         
-        // Approve tutorial contract to spend USDC
-        uint256 mintAmount = 1000 * 1e6; // 1000 USDC
-        ERC20(USDC).approve(address(tutorial), mintAmount);
+        // Check if we're in basket mode
+        bool isBasketMode = factory.isBasketModeEnabled(true);
+        console2.log("Is basket mode enabled:", isBasketMode);
 
-        // Mint Honey using USDC
-        uint256 honeyMinted = tutorial.mintBasicMode(USDC, mintAmount);
+        // Preview the mint amount
+        uint256 mintAmount = 1000 * 1e6; // 1000 USDT
+        uint256 expectedHoney = factoryReader.previewMint(USDT, mintAmount);
+        console2.log("Expected Honey from mint:", expectedHoney);
+        
+        // Approve the tutorial contract to spend both USDT and USDC
+        ERC20(USDT).approve(address(minter), mintAmount);
+        ERC20(USDC).approve(address(minter), mintAmount);
+
+        // Mint Honey using appropriate mode
+        uint256 honeyMinted;
+        if (isBasketMode) {
+            honeyMinted = minter.mintBasketMode(USDT, mintAmount);
+        } else {
+            honeyMinted = minter.mintBasicMode(USDT, mintAmount);
+        }
         
         // Verify Honey was minted
         assertGt(honeyMinted, 0, "Should have minted some Honey");
         assertEq(honey.balanceOf(user), honeyMinted, "User should have received Honey");
-
+        
         vm.stopPrank();
     }
 
     function test_BasicModeRedeeming() public {
-        vm.startPrank(user);
-        
         // First mint some Honey
-        uint256 mintAmount = 1000 * 1e6; // 1000 USDC
-        ERC20(USDC).approve(address(tutorial), mintAmount);
-        uint256 honeyMinted = tutorial.mintBasicMode(USDC, mintAmount);
+        test_BasicModeMinting();
         
-        // Now redeem the Honey
-        honey.approve(address(tutorial), honeyMinted);
-        uint256[] memory redeemedAmount = tutorial.redeemBasicMode(USDC, honeyMinted);
-        
-        // Verify redemption
-        assertGt(redeemedAmount[0], 0, "Should have redeemed some USDC");
-        
-        vm.stopPrank();
-    }
-
-    function test_BasketModeMinting() public {
-        // Setup basket mode
-        vm.startPrank(MANAGER);
-        factory.setForcedBasketMode(true);
-        vm.stopPrank();
-
         vm.startPrank(user);
         
-        // Approve tutorial contract to spend tokens
-        uint256 mintAmount = 1000 * 1e6; // 1000 USDC
-        ERC20(USDC).approve(address(tutorial), mintAmount);
+        // Check if we're in basket mode
+        bool isBasketMode = factory.isBasketModeEnabled(false);
+        console2.log("Is basket mode enabled for redeem:", isBasketMode);
 
-        // Mint Honey in basket mode
-        uint256 honeyMinted = tutorial.mintBasketMode(USDC, mintAmount);
-        
-        // Verify Honey was minted
-        assertGt(honeyMinted, 0, "Should have minted some Honey");
-        assertEq(honey.balanceOf(user), honeyMinted, "User should have received Honey");
+        // Get user's Honey balance
+        uint256 honeyBalance = honey.balanceOf(user);
+        console2.log("User's Honey balance:", honeyBalance);
 
-        vm.stopPrank();
-    }
+        // Preview redeem amount
+        uint256[] memory expectedRedeemAmounts;
+        if (isBasketMode) {
+            expectedRedeemAmounts = factoryReader.previewRedeemBasketMode(honeyBalance);
+            console2.log("Expected USDT from redeem:", expectedRedeemAmounts[0]);
+            console2.log("Expected USDC from redeem:", expectedRedeemAmounts[1]);
+        } else {
+            expectedRedeemAmounts = new uint256[](1);
+            expectedRedeemAmounts[0] = factoryReader.previewRedeem(USDT, honeyBalance);
+            console2.log("Expected USDT from redeem:", expectedRedeemAmounts[0]);
+        }
 
-    function test_BasketModeRedeeming() public {
-        // Setup basket mode
-        vm.startPrank(MANAGER);
-        factory.setForcedBasketMode(true);
-        vm.stopPrank();
+        // Approve tutorial to spend Honey
+        honey.approve(address(minter), honeyBalance);
 
-        vm.startPrank(user);
-        
-        // First mint some Honey in basket mode
-        uint256 mintAmount = 1000 * 1e6; // 1000 USDC
-        ERC20(USDC).approve(address(tutorial), mintAmount);
-        uint256 honeyMinted = tutorial.mintBasketMode(USDC, mintAmount);
-        
-        // Now redeem the Honey
-        honey.approve(address(tutorial), honeyMinted);
-        uint256[] memory redeemedAmounts = tutorial.redeemBasketMode(USDC, honeyMinted);
-        
+        // Redeem Honey
+        uint256[] memory redeemedAmounts;
+        if (isBasketMode) {
+            redeemedAmounts = minter.redeemBasketMode(USDT, honeyBalance);
+        } else {
+            redeemedAmounts = minter.redeemBasicMode(USDT, honeyBalance);
+        }
+
         // Verify redemption
-        assertGt(redeemedAmounts.length, 0, "Should have redeemed some collateral");
+        assertEq(redeemedAmounts.length, 
+            expectedRedeemAmounts.length, 
+            "Should have received expected number of tokens");
+
+        for (uint256 i = 0; i < redeemedAmounts.length; i++) {
+            assertApproxEqRel(redeemedAmounts[i], 
+                expectedRedeemAmounts[i], 
+                0.01e18, 
+                "Redeemed amount should match preview");
+        }
         
         vm.stopPrank();
     }
 
-    function test_CheckBasketMode() public {
-        // Initially should be in basic mode
-        assertFalse(tutorial.isBasketMode(true), "Should not be in basket mode for minting");
-        assertFalse(tutorial.isBasketMode(false), "Should not be in basket mode for redeeming");
-
-        // Enable basket mode
-        vm.prank(MANAGER);
-        factory.setForcedBasketMode(true);
-
-        // Should now be in basket mode
-        assertTrue(tutorial.isBasketMode(true), "Should be in basket mode for minting");
-        assertTrue(tutorial.isBasketMode(false), "Should be in basket mode for redeeming");
-    }
 } 
